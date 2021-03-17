52a1a53719d5ce6dbfea2c72a42dc631d32dab3b
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-03-17 19:52:20 +0000

    Add images/webfig*.(pdf/eps/R)

---
+++ images/webfig2.R
@@ -0,0 +1,31 @@
+# Reproduce images/webfig2.pdf
+
+## Load the relevant libraries
+library(dplyr)
+library(ggplot2)
+library(readr)
+
+## Create the plot
+p <-
+    readr::read_csv("data/webfig2.csv") %>%
+    dplyr::mutate(
+        N = factor(N),
+        l = factor(l, labels = c("low", "medium", "large")),
+        H0 = factor(H0),
+        H1 = factor(H1)
+    ) %>%
+    ggplot(aes(H0, H1)) +
+    geom_tile(aes(fill = value), color = "grey90") +
+    facet_grid(l ~ N) +
+    labs(
+        y = "Hypothesis, by discrepancy between the group distributions",
+        x = expression(paste("True hypothesis, by sample size"))
+    ) +
+    theme_classic() +
+    theme(legend.position = "top") +
+    scale_fill_gradient(low = "white", high = "black") +
+    theme(axis.text.x = element_text(angle = 90))
+
+## Save the plot in pdf format
+ggsave("images/webfig2.pdf", p, width = 6, height = 5)
+ggsave("images/webfig2.eps", p, width = 6, height = 5)
